55fecabe2aa293cfd35c464e370b574acb0dbb28
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-06-22 10:27:22 +0200

    Add page numbers and start on removeComments and removeWhitespaces

--- src/clone.ts
+++ src/clone.ts
@@ -5,28 +5,28 @@ import path from "path"
 import { execSync } from "child_process"
 
 import git from "simple-git"
-import PDFDocument from "pdfkit"
+import PDFDocument, { addPage } from "pdfkit"
 import { default as hljs } from "highlight.js"
 import { htmlToJson } from "./syntax"
 import { isBinaryFileSync } from "isbinaryfile"
 
 //@ts-ignore
-import type chalkType from "chalk";
+import type chalkType from "chalk"
 //@ts-ignore
-import type inquirerType from "inquirer";
+import type inquirerType from "inquirer"
 //@ts-ignore
-import type oraType from "ora";
+import type oraType from "ora"
 
 // TODO IDEAS
 // TODO add option to condiotionaly remove comments from code
 // TODO add option to condiotionaly remove empty lines from code
-// TODO add option to condiotionaly add line numbers to code
-// TODO add option to condiotionaly add linting to code
 // TODO add option to make one pdf per file
+// TODO add page numbers to pdf
+// TODO add table of contents to pdf
 
-let chalk: typeof chalkType;
-let inquirer: typeof inquirerType;
-let ora: typeof oraType;
+let chalk: typeof chalkType
+let inquirer: typeof inquirerType
+let ora: typeof oraType
 
 const spinnerPromise = import("ora").then((oraModule) => {
   ora = oraModule.default
@@ -36,7 +36,8 @@ const spinnerPromise = import("ora").then((oraModule) => {
 Promise.all([
   import("chalk").then((chalkModule) => chalkModule.default),
   import("inquirer").then((inquirerModule) => inquirerModule.default),
-  spinnerPromise])
+  spinnerPromise,
+])
   .then(([chalkModule, inquirerModule, spinner]) => {
     chalk = chalkModule
     inquirer = inquirerModule
@@ -52,122 +53,127 @@ Promise.all([
 
 async function askForRepoUrl() {
   const questions: {
-    type?: string,
+    type?: string
     name: [
       "repoUrl",
       "optionalExcludedNames",
       "optionalExcludedExtensions",
       "addLineNumbers",
-      "addLinting",
+      "addHighlighting",
       "removeComments",
       "removeEmptyLines",
       "onePdfPerFile",
       "outputFileName",
       "outputFolderName",
       "keepRepo"
-    ][number],
-    message: string,
-    validate?: (value: string) => boolean | string,
-    filter?: (value: string) => boolean | string | string[],
-    choices?: string[],
-    default?: string | string[],
-    when?: (answers: any) => boolean,
+    ][number]
+    message: string
+    validate?: (value: string) => boolean | string
+    filter?: (value: string) => boolean | string | string[]
+    choices?: string[]
+    default?: string | string[]
+    when?: (answers: any) => boolean
   }[] = [
-      {
-        name: "repoUrl",
-        message: "Please provide a GitHub repository URL:",
-        validate: function (value: string) {
-          var pass = value.match(
-            /^https:\/\/github.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/
-          )
-          if (pass) {
-            return true
-          }
-          return "Please enter a valid GitHub repository URL."
-        },
+    {
+      name: "repoUrl",
+      message: "Please provide a GitHub repository URL:",
+      validate: function (value: string) {
+        var pass = value.match(
+          /^https:\/\/github.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/
+        )
+        if (pass) {
+          return true
+        }
+        return "Please enter a valid GitHub repository URL."
       },
-      {
-        name: "optionalExcludedNames",
-        message:
-          "Please provide a list of file names to exclude, separated by commas:",
-        filter: function (value: string) {
-          return value.split(",").map((v) => v.trim())
-        },
+    },
+    {
+      name: "optionalExcludedNames",
+      message:
+        "Please provide a list of file names to exclude, separated by commas:",
+      filter: function (value: string) {
+        return value.split(",").map((v) => v.trim())
       },
-      {
-        name: "optionalExcludedExtensions",
-        message:
-          "Please provide a list of file extensions to exclude, separated by commas:",
-        filter: function (value: string) {
-          return value.split(",").map((v: string) => v.trim())
-        },
+    },
+    {
+      name: "optionalExcludedExtensions",
+      message:
+        "Please provide a list of file extensions to exclude, separated by commas:",
+      filter: function (value: string) {
+        return value.split(",").map((v: string) => v.trim())
       },
-      {
-        name: "addLineNumbers",
-        message: "Do you want to add line numbers to the PDF?",
-        choices: ["Yes", "No"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "addLineNumbers",
+      message: "Do you want to add line numbers to the PDF?",
+      choices: ["Yes", "No"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-      {
-        name: "addLinting",
-        message: "Do you want to add linting to the PDF?",
-        choices: [/*"Yes",*/ "No"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "addHighlighting",
+      message: "Do you want to add highlighting to the PDF?",
+      choices: ["Yes", "No"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-      {
-        name: "removeComments",
-        message: "Do you want to remove comments from the PDF?",
-        choices: [/*"Yes",*/ "No"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "removeComments",
+      message: "Do you want to remove comments from the PDF?",
+      choices: ["No", "Yes"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-      {
-        name: "removeEmptyLines",
-        message: "Do you want to remove empty lines from the PDF?",
-        choices: [/*"Yes",*/ "No"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "removeEmptyLines",
+      message: "Do you want to remove empty lines from the PDF?",
+      choices: ["No", "Yes"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-      {
-        name: "onePdfPerFile",
-        message: "Do you want to make one PDF per file?",
-        choices: [/*"Yes",*/ "No"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "onePdfPerFile",
+      message: "Do you want to make one PDF per file?",
+      choices: ["No", "Yes"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-      {
-        name: "outputFileName",
-        message: "Please provide an output file name:",
-        default: "output.pdf",
-        when(answers: { onePdfPerFile: any }) {
-          return !answers.onePdfPerFile
-        },
+    },
+    {
+      name: "outputFileName",
+      message: "Please provide an output file name:",
+      default: "output.pdf",
+      when(answers: { onePdfPerFile: any }) {
+        return !answers.onePdfPerFile
       },
-      {
-        name: "outputFolderName",
-        message: "Please provide an output folder name:",
-        default: "./output",
-        when(answers: { onePdfPerFile: any }) {
-          return answers.onePdfPerFile
-        },
+    },
+    {
+      name: "outputFolderName",
+      message: "Please provide an output folder name:",
+      default: "./output",
+      when(answers: { onePdfPerFile: any }) {
+        return answers.onePdfPerFile
       },
-      {
-        type: "list",
-        name: "keepRepo",
-        message: "Do you want to keep the cloned repository?",
-        choices: ["No", "Yes"],
-        filter: function (val: string) {
-          return val.toLowerCase() === "yes"
-        },
+    },
+    {
+      type: "list",
+      name: "keepRepo",
+      message: "Do you want to keep the cloned repository?",
+      choices: ["No", "Yes"],
+      filter: function (val: string) {
+        return val.toLowerCase() === "yes"
       },
-    ]
+    },
+  ]
 
   console.log(
     chalk.cyanBright(`
@@ -190,7 +196,7 @@ Welcome to Repo-to-PDF! Let's get started...
     answers.optionalExcludedNames,
     answers.optionalExcludedExtensions,
     answers.addLineNumbers,
-    answers.addLinting,
+    answers.addHighlighting,
     answers.removeComments,
     answers.removeEmptyLines,
     answers.onePdfPerFile,
@@ -205,7 +211,7 @@ async function main(
   optionalExcludedNames: string[],
   optionalExcludedExtensions: string[],
   addLineNumbers: any,
-  addLinting: any,
+  addHighlighting: any,
   removeComments: any,
   removeEmptyLines: any,
   onePdfPerFile: any,
@@ -215,7 +221,10 @@ async function main(
 ) {
   const gitP = git()
   const tempDir = "./tempRepo"
-  const doc = new PDFDocument()
+  const doc = new PDFDocument({
+    bufferPages: true,
+    autoFirstPage: false,
+  })
   doc.pipe(fs.createWriteStream(outputFileName))
 
   let fileCount = 0
@@ -229,8 +238,26 @@ async function main(
       appendFilesToPdf(
         tempDir,
         optionalExcludedNames,
-        optionalExcludedExtensions
+        optionalExcludedExtensions,
+        removeComments
       ).then(() => {
+        //Global Edits to All Pages (Header/Footer, etc)
+        let pages = doc.bufferedPageRange()
+        for (let i = 0; i < pages.count; i++) {
+          doc.switchToPage(i)
+
+          //Footer: Add page number
+          let oldBottomMargin = doc.page.margins.bottom
+          doc.page.margins.bottom = 0 //Dumb: Have to remove bottom margin in order to write into it
+          doc.text(
+            `Page: ${i + 1} of ${pages.count}`,
+            0,
+            doc.page.height - oldBottomMargin / 2, // Centered vertically in bottom margin
+            { align: "center" }
+          )
+          doc.page.margins.bottom = oldBottomMargin // ReProtect bottom margin
+        }
+
         doc.end()
         spinner.succeed(
           chalk.greenBright(`PDF created with ${fileCount} files processed.`)
@@ -251,15 +278,13 @@ async function main(
   async function appendFilesToPdf(
     directory: string,
     optionalExcludedNames: string[],
-    optionalExcludedExtensions: string[]
+    optionalExcludedExtensions: string[],
+    removeComments = false
   ) {
-    console.log(chalk.blueBright(`Processing ${directory}...`));
     const files = await fsPromises.readdir(directory)
-    console.log(chalk.blueBright(`Found ${files.length} files in ${directory}`));
-    
+
     for (let file of files) {
       const filePath = path.join(directory, file)
-      console.log(chalk.blueBright(`Processing ${filePath}...`));
       const stat = await fsPromises.stat(filePath)
 
       const excludedNames = [
@@ -315,6 +340,7 @@ async function main(
           data = data.replace(/Ð/g, "\n")
           data = data.replace(/\r\n/g, "\n")
           data = data.replace(/\r/g, "\n")
+          data = data.replace(/uþs/g, "")
 
           doc
             .addPage()
@@ -323,29 +349,43 @@ async function main(
             .text(`${fileName}\n\n`, { lineGap: 4 })
 
           const extension = path.extname(filePath).replace(".", "")
-          const highlightedCode = hljs.highlight(data, { language: extension }).value
-          const hlData = htmlToJson(highlightedCode);
-          let lineNum = 1;
-          const lineNumWidth = (hlData.filter((d) => d.text === "\n")).length.toString().length;
+          let highlightedCode
+          try {
+            highlightedCode = hljs.highlight(data, {
+              language: addHighlighting ? extension : "plaintext",
+            }).value
+          } catch (error) {
+            highlightedCode = hljs.highlight(data, {
+              language: "plaintext",
+            }).value
+          }
+          const hlData = htmlToJson(highlightedCode, removeComments)
+          let lineNum = 1
+          const lineNumWidth = hlData
+            .filter((d) => d.text === "\n")
+            .length.toString().length
           for (let i = 0; i < hlData.length; i++) {
-            const { text, color } = hlData[i];
+            const { text, color } = hlData[i]
             if (i == 0 || hlData[i - 1]?.text === "\n")
-              doc.text(String(lineNum++).padStart(lineNumWidth, " ") + " ", { continued: true, textIndent: 0 });
+              if (addLineNumbers) {
+                doc.text(String(lineNum++).padStart(lineNumWidth, " ") + " ", {
+                  continued: true,
+                  textIndent: 0,
+                })
+              }
+            if (color) doc.fillColor(color)
+            else doc.fillColor("black")
 
-            if (color) doc.fillColor(color);
-            else doc.fillColor("black");
-
-            if (text !== "\n") doc.text(text, { continued: true });
-            else doc.text(text);
-
+            if (text !== "\n") doc.text(text, { continued: true })
+            else doc.text(text)
           }
         }
       } else if (stat.isDirectory()) {
-        console.log(chalk.blueBright(`Processing new directory: ${filePath}...`));
         await appendFilesToPdf(
           filePath,
           optionalExcludedNames,
-          optionalExcludedExtensions
+          optionalExcludedExtensions,
+          removeComments
         )
       }
     }
--- src/syntax.ts
+++ src/syntax.ts
@@ -1,33 +1,36 @@
 import { decode } from "html-entities"
 
 /**
- * @param {string} htmlCode 
+ * @param {string} htmlCode
  */
-export function htmlToJson(htmlCode: string): { text: string, color?: string }[] {
-  const originalCode = htmlCode;
+export function htmlToJson(
+  htmlCode: string,
+  removeComments: boolean
+): { text: string; color?: string }[] {
+  const originalCode = htmlCode
   /**
    * @type {{text: string, color?: string}[]}
    */
-  const data: { text: string, color?: string }[] = [];
-  const elementRegex = /^<span\s+class="hljs-([^"]+)"[^>]*>([^<]*)(?:<\/span>)?/;
-  const nonelementRegex = /[^<]*/;
+  const data: { text: string; color?: string }[] = []
+  const elementRegex = /^<span\s+class="hljs-([^"]+)"[^>]*>([^<]*)(?:<\/span>)?/
+  const nonelementRegex = /[^<]*/
   while (htmlCode) {
-    const match = htmlCode.match(elementRegex);
+    const match = htmlCode.match(elementRegex)
     if (match) {
-      const fullText = match[0];
-      const cls = match[1];
-      const text = match[2];
-      let color = "black";
+      const fullText = match[0]
+      const cls = match[1]
+      const text = match[2]
+      let color = "black"
       // const color = cls;
-      const type = cls.split(" ")[0].toLowerCase() ?? "unknown";
+      const type = cls.split(" ")[0].toLowerCase() ?? "unknown"
       switch (type) {
         case "comment":
-          color = "#697070";
-          break;
+          color = "#697070"
+          break
         case "punctuation":
         case "tag":
-          color = "#444a";
-          break;
+          color = "#444a"
+          break
         case "attribute":
         case "doctag":
         case "keyword":
@@ -35,8 +38,8 @@ export function htmlToJson(htmlCode: string): { text: string, color?: string }[]
         case "keyword":
         case "name":
         case "selector-tag":
-          color = "#32a2cb";
-          break;
+          color = "#32a2cb"
+          break
         case "deletion":
         case "number":
         case "quote":
@@ -47,8 +50,8 @@ export function htmlToJson(htmlCode: string): { text: string, color?: string }[]
         case "type":
         case "section":
         case "title":
-          color = "#800";
-          break;
+          color = "#800"
+          break
         case "link":
         case "operator":
         case "regexp":
@@ -57,60 +60,80 @@ export function htmlToJson(htmlCode: string): { text: string, color?: string }[]
         case "symbol":
         case "template-variable":
         case "variable":
-          color = "#ab5656";
-          break;
+          color = "#ab5656"
+          break
         case "literal":
-          color = "#695";
-          break;
+          color = "#695"
+          break
         case "addition":
         case "built_in":
         case "bullet":
         case "code":
-          color = "#397300";
-          break;
+          color = "#397300"
+          break
         case "meta":
-          color = "#1f7199";
-          break;
+          color = "#1f7199"
+          break
         case "string":
-          color = "#38a";
-          break;
+          color = "#38a"
+          break
       }
       // console.log({ type, text, color, fullText });
-      data.push({ text: decode(text), color });
-      htmlCode = htmlCode.slice(fullText.length);
-    }
-    else if (htmlCode.startsWith("</span>")) { // Failed ending from hljs
-      const text = "</span>";
-      data.push({ text: "" }); // Empty text on purpose
-      htmlCode = htmlCode.slice(text.length);
-    }
-    else if (htmlCode.startsWith("\n")) {
-      const text = "\n";
-      htmlCode = htmlCode.slice(1);
-      data.push({ text });
-    }
-    else {
-      const match = htmlCode.match(nonelementRegex);
-      const text = match![0];
-      htmlCode = htmlCode.slice(text.length);
-      data.push({ text: decode(text) });
+      data.push({ text: decode(text), color })
+      htmlCode = htmlCode.slice(fullText.length)
+    } else if (htmlCode.startsWith("</span>")) {
+      // Failed ending from hljs
+      const text = "</span>"
+      data.push({ text: "" }) // Empty text on purpose
+      htmlCode = htmlCode.slice(text.length)
+    } else if (htmlCode.startsWith("\n")) {
+      const text = "\n"
+      htmlCode = htmlCode.slice(1)
+      data.push({ text })
+    } else {
+      const match = htmlCode.match(nonelementRegex)
+      const text = match![0]
+      htmlCode = htmlCode.slice(text.length)
+      data.push({ text: decode(text) })
     }
   }
 
   /**
    * @type {{text: string, color?: string}[]}
    */
-  const fixedData: { text: string, color?: string }[] = [];
-  // Fix newlines
+  const fixedData: { text: string; color?: string }[] = []
+
+  // BUG: REMOVE COMMENTS not working properly
+  // TODO: Remove new lines if removeNewLines is true
+  // Fix newlines and remove comments
+  let skipLine = false
   for (let i = 0; i < data.length; i++) {
-    const { text, color } = data[i];
-    const lines = text.split("\n");
+    const { text, color } = data[i]
+    const lines = text.split("\n")
+
     for (let j = 0; j < lines.length; j++) {
-      const line = lines[j];
-      if (j > 0) fixedData.push({ text: "\n" });
-      fixedData.push({ text: line, color });
+      const line = lines[j]
+
+      // Skip the current line if a comment was encountered and removeComments is true
+      if (removeComments && skipLine) {
+        skipLine = false
+        if (j !== lines.length - 1) {
+          // Don't skip if it's the last element (which is usually empty after split)
+          continue
+        }
+      }
+
+      // Mark for skipping the next line if a comment is encountered
+      if (color === "#697070") {
+        // assuming color '#697070' indicates comments
+        skipLine = true
+      }
+
+      if (j > 0) fixedData.push({ text: "\n" })
+
+      fixedData.push({ text: line, color })
     }
   }
 
-  return fixedData;
+  return fixedData
 }
